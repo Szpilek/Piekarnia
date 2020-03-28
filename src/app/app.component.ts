5a45cad876d274bd6dc32bd3ddf86d40d8c6b147
Your Name <you@example.com>
2020-03-29 00:48:42 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+enum Page {
+  BREADS,
+  HISTORY,
+  ABOUT,
+  CONTACT,
+  MAIN
+
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +17,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'piekarnia';
+  currentPage: Page = Page.MAIN;
+  changePage(page: string) {
+    switch (page) {
+      case 'breads':
+        this.currentPage = Page.BREADS;
+        break;
+      case 'history':
+        this.currentPage = Page.HISTORY;
+        break;
+      case 'contact':
+        this.currentPage = Page.CONTACT;
+        break;
+      case 'about':
+        this.currentPage = Page.ABOUT;
+        break;
+      case 'main':
+        this.currentPage = Page.MAIN;
+        break;
+      default:
+        this.currentPage = Page.MAIN;
+        break;
+    }
+  }
+
+  isCurrentPageMain() {
+    return this.currentPage == Page.MAIN;
+  }
+
+  isCurrentPageBreads() {
+    return this.currentPage == Page.BREADS;
+  }
+
+  isCurrentPageHistory() {
+    return this.currentPage == Page.HISTORY;
+  }
+
+  isCurrentPageContact() {
+    return this.currentPage == Page.CONTACT;
+  }
+
+  isCurrentPageAbout() {
+    return this.currentPage == Page.ABOUT;
+  }
+
+
 }
---
+++ src/app/bread-card/bread-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bread-card',
+  templateUrl: './bread-card.component.html',
+  styleUrls: ['./bread-card.component.css']
+})
+export class BreadCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
